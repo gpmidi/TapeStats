4d74528a0f556524649f323c0dac3ff64e8e2601
Paulson McIntyre <paul@gpmidi.net>
2021-01-18 18:09:19 -0500

    Switched to zerolog

--- main.go
+++ main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"github.com/gin-contrib/logger"
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
 	"os"
 )
 
 func main() {
-	log := logrus.New().WithField("program", "tape-stats")
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if gin.IsDebugging() {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	log.Logger = log.Output(
+		zerolog.ConsoleWriter{
+			Out:     os.Stderr,
+			NoColor: false,
+		},
+	).With().
+		Str("program", "tape-stats").
+		Logger()
+
+	// Local logger
+	l := log.With().Logger()
+
+	r := gin.New()
+	r.Use(logger.SetLogger())
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	log = log.WithField("listen.port", port)
-	log.Info("Going to listen on port")
-
-	r := gin.Default()
+	l = l.With().Str("listen.port", port).Logger()
+	l.Info().Msg("Going to listen on port")
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -24,6 +46,6 @@ func main() {
 		})
 	})
 	if err := r.Run("0.0.0.0:" + port); err != nil {
-		log.WithError(err).Panic("Problem running server")
+		log.Panic().Err(err).Msg("Problem running server")
 	}
 }
