3900b9ed7efa1193e4a4aa0a563bcc2af1a2f0ab
Paulson McIntyre <paul@gpmidi.net>
2021-01-18 21:54:59 -0500

    Added account register route

--- ts/routes.go
+++ ts/routes.go
@@ -9,6 +9,9 @@ func (ts *TapeStatsApp) AddRoutes(r *gin.Engine) {
 	r.GET("/", ts.IndexHandler)
 	r.GET("/ping", ts.PingHandler)
 
+	// Auth
+	r.POST("/auth/register", ts.RegisterAccountHandler)
+
 	// Submission handlers
 
 	// Stats handlers
