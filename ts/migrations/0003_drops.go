7062cef93731943f145b29fc0573140c3becda37
Paulson McIntyre <paul@gpmidi.net>
2021-01-18 22:29:16 -0500

    Added a few more migrations

---
+++ ts/migrations/0003_drops.go
@@ -0,0 +1,18 @@
+package migrations
+
+import (
+	"github.com/go-pg/migrations/v8"
+)
+
+func init() {
+	migrations.MustRegisterTx(
+		func(db migrations.DB) error {
+			_, err := db.Exec(`DROP TABLE test_table;`)
+			return err
+		},
+		func(db migrations.DB) error {
+			_, err := db.Exec(`DROP TABLE accounts;`)
+			return err
+		},
+	)
+}
