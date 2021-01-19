2cab16f29dedcb4b83ee30a8d11cfe31326b66e8
Paulson McIntyre <paul@gpmidi.net>
2021-01-18 23:43:33 -0500

    Filled in register account handler

--- ts/accounts.go
+++ ts/accounts.go
@@ -1,7 +1,9 @@
 package ts
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/gpmidi/TapeStats/ts/tsdb"
 )
 
 func (ts *TapeStatsApp) RegisterAccountHandler(c *gin.Context) {
@@ -10,10 +12,39 @@ func (ts *TapeStatsApp) RegisterAccountHandler(c *gin.Context) {
 		ts.Log.Error().Err(c.Error(err)).Msg("Problem with getting ts")
 		return
 	}
+	log := li.Log
+
+	act := &tsdb.Account{}
+	pw, err := act.CreateSetPassword()
+	if err != nil {
+		log.Error().Err(c.Error(err)).Msg("Problem with getting random password and/or hash")
+		return
+	}
+
+	res, err := ts.DB.Model(act).Insert()
+	if err != nil {
+		log.Error().Err(c.Error(err)).Msg("Problem saving account to db")
+		return
+	}
+	log = log.With().Int("rows.returned", res.RowsReturned()).Int("rows.affected", res.RowsAffected()).Logger()
+	if res.RowsAffected() != 1 {
+		err = errors.New("one row not affected")
+		log.Error().Err(c.Error(err)).Msg("Problem saving account to db - Rows affected is odd")
+		return
+	}
+	if res.RowsReturned() != 1 {
+		err = errors.New("one row not returned")
+		log.Error().Err(c.Error(err)).Msg("Problem saving account to db - rows returned is odd")
+		return
+	}
 
 	c.JSON(200, gin.H{
-		"message": "pong",
+		"message": "Account Created",
+		"account": gin.H{
+			"id":       act.Id,
+			"password": pw,
+		},
 		"request": li.Data(),
 	})
-	li.Log.Debug().Msg("Ping-Pong")
+	li.Log.Info().Msg("Account Created")
 }
--- ts/tsdb/tbl_accounts.go
+++ ts/tsdb/tbl_accounts.go
@@ -1,18 +1,49 @@
 package tsdb
 
 import (
+	"crypto/rand"
 	"errors"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/bcrypt"
+	"math/big"
 	"time"
 )
 
 type Account struct {
 	tableName struct{}  `pg:"accounts,discard_unknown_columns"`
 	Id        string    `pg:"id,pk,type:uuid,default:gen_random_uuid()"`
-	created   time.Time `pg:",notnull"`
-	modified  time.Time `pg:",notnull"`
-	hashed    string
+	Created   time.Time `pg:"created,notnull"`
+	Modified  time.Time `pg:"modified,notnull"`
+	Hashed    string    `pg:"hashed"`
+}
+
+func (a *Account) CreatePassword() (string, error) {
+	result := ""
+	for {
+		if len(result) >= viper.GetInt("passwords.length") {
+			return result, nil
+		}
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(127)))
+		if err != nil {
+			return "", err
+		}
+		n := num.Int64()
+		// Make sure that the number/byte/letter is inside
+		// the range of printable ASCII characters (excluding space and DEL)
+		if n > 32 && n < 127 {
+			result += string(n)
+		}
+	}
+}
+func (a *Account) CreateSetPassword() (string, error) {
+	password, err := a.CreatePassword()
+	if err != nil {
+		return "", err
+	}
+	if err := a.SetPassword(password); err != nil {
+		return password, err // Return password in case the change does somehow happen
+	}
+	return password, nil
 }
 
 func (a *Account) SetPassword(passwd string) error {
@@ -21,16 +52,16 @@ func (a *Account) SetPassword(passwd string) error {
 		return err
 	}
 
-	a.hashed = (string)(hashedPassword)
+	a.Hashed = (string)(hashedPassword)
 
 	return nil
 }
 
 func (a *Account) VerifyPassword(passwd string) (bool, error) {
-	if passwd == "" || a.hashed == "" {
-		return false, errors.New("password and/or hashed can't be empty")
+	if passwd == "" || a.Hashed == "" {
+		return false, errors.New("password and/or Hashed can't be empty")
 	}
-	err := bcrypt.CompareHashAndPassword(([]byte)(a.hashed), ([]byte)(passwd))
+	err := bcrypt.CompareHashAndPassword(([]byte)(a.Hashed), ([]byte)(passwd))
 	if err != nil {
 		return false, err
 	}
@@ -39,4 +70,5 @@ func (a *Account) VerifyPassword(passwd string) (bool, error) {
 
 func init() {
 	viper.SetDefault("passwords.extracost", 8)
+	viper.SetDefault("passwords.length", 16)
 }
